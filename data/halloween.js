9f6a74220bbf440bafa4f0353097be4038e97fc5
Mandy_Mascetti <armascetti@gmail.com>
2021-10-26 10:29:46 -0400

    added views/data folders

---
+++ data/halloween.js
@@ -0,0 +1,5 @@
+const villains = [
+  {text: 'Freddy Kreuger'},
+  {text: 'Chucky'},
+  {text: 'Michael Myers'}
+]
--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 import express from 'express'
-
 const app = express()
 
+//Configure the app 
+app.set('view engine', 'ejs')
 
-app.listen(3000, function(){
-  console.log('listening on port 3000')
+app.get('/', function(req, res) {
+  res.send('<h1>hello, friend</h1>')
+})
+
+
+app.get('/home', function(req,res){
+res.render('home')
+})
+
+
+app.listen(4000, function(){
+  console.log('listening on port 4000')
 })
