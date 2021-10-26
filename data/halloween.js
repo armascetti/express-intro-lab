c2ec449e6f1870a661a57d0d91c854781634ad88
Mandy_Mascetti <armascetti@gmail.com>
2021-10-26 11:06:18 -0400

    imported files/created index.ejs file

--- data/halloween.js
+++ data/halloween.js
@@ -1,5 +1,25 @@
 const villains = [
-  {text: 'Freddy Kreuger'},
-  {text: 'Chucky'},
-  {text: 'Michael Myers'}
+  {text: 'Freddy Kreuger', done: true, _id: 125223},
+  {text: 'Chucky', done: true, _id: 127904},
+  {text: 'Michael Myers', done: true, _id: 139608}
 ]
+
+const find = (conditions, callback) => {
+  // see if this works, if not, execute the code in the catch block
+  try {
+    // make sure that conditions is an object - if not throw a TypeError
+    if (!(conditions instanceof Object)){
+      throw new TypeError('Please pass in an object')
+    }
+    // If the object is empty, return all the todos
+    if (Object.keys(conditions).length === 0) return callback(null, villains)
+	// deal with errors
+  } catch (error) {
+    console.log(error)
+    callback(error, [])
+  }
+}
+
+export { 
+	find
+}
--- server.js
+++ server.js
@@ -1,19 +1,38 @@
+//import modules
 import express from 'express'
+import * as halloween from './data/halloween.js'
+
+
+//create express application
 const app = express()
+
 
 //Configure the app 
 app.set('view engine', 'ejs')
 
+
+//Mount routes 
+//localhost:4000/
 app.get('/', function(req, res) {
-  res.send('<h1>hello, friend</h1>')
+  res.redirect('/home')
 })
 
-
+//localhost:4000/home
 app.get('/home', function(req,res){
-res.render('home')
+res.send('<h1>The Best Halloween Villains</h1>')
 })
 
+//localhost:4000/halloween 
+app.get('/halloween', function(req, res) {
+halloween.find({}, function(error, villains) {
+    res.render('halloween/index', {
+      villains: villains,
+      error: error
+    })
+  })
+})
 
+//tell our app to listen to port 4000 
 app.listen(4000, function(){
   console.log('listening on port 4000')
 })
